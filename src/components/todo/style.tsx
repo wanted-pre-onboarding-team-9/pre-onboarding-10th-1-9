bbce9c6dccf290d7943287fb1f8ac56e41abaa96
sojinjang <jsojin6119@gmail.com>
2023-04-28 03:56:52 +0900

    :lipstick: style: 투두 컴포넌트 스타일링

--- src/components/todo/style.tsx
+++ src/components/todo/style.tsx
@@ -1,18 +1,66 @@
 import styled from 'styled-components';
+import { TextInputStyleProps } from '../../@types/todo';
 
-const CheckBoxContainer = styled.div`
+export const CheckBoxContainer = styled.div`
   width: 35px;
   height: 35px;
-  border: 2px solid red;
 `;
 
-const Button = styled.button`
+export const ButtonContainer = styled.button`
   background-color: #3f51b5;
-  cursor: ${({ disabled }) => (disabled ? 'not-allowed' : 'pointer')};
   color: #fff;
-  border-radius: 0.375 rem;s
-  padding: 0.875rem  0.625rem;
+  border: none;
+  cursor: pointer;
+  width: 3.125rem;
+  height: 40px;
+  border-radius: 0.313rem;
+  padding: 0.875rem 0.625rem;
+  font-size: 0.875rem;
+  font-weight: 600;
+  box-shadow: 0 1px 2px 0 rgb(0 0 0 / 0.05);
+  transition: 0.3s;
 
+  &:disabled {
+    opacity: 0.5;
+    cursor: not-allowed;
+  }
+
+  &:focus:not(.focus-visible) {
+    outline: none;
+  }
+  &:focus-visible {
+    outline: 2px solid;
+    outline-offset: 2px;
+    outline-color: #4f46e5;
+  }
 `;
 
-export default CheckBoxContainer;
+export const TextInputContainer = styled.div<TextInputStyleProps>`
+  width: ${(props) => props.width};
+  height: 40px;
+
+  & > input {
+    border: none;
+    border-radius: 5px;
+    box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.1);
+    box-sizing: border-box;
+    width: 100%;
+    height: 100%;
+    padding: 5px 10px;
+  }
+`;
+
+export const CreateTodoContainer = styled.div`
+  margin-bottom: 40px;
+
+  & > form {
+    display: flex;
+    gap: 0px 15px;
+  }
+  button {
+    width: 3.125rem;
+    border-radius: 0.313rem;
+    border: none;
+    padding: 0px;
+  }
+`;
