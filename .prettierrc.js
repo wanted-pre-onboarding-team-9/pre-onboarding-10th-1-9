5a208cdec868590726655bb3ae2d0a3681501769
Parksangwoo <psu8430@gmail.com>
2023-04-27 15:13:02 +0900

    :sparkles: feat: eslint, prettier, husky 세팅 추가

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  parser: 'typescript',
+  singleQuote: true,
+  tabWidth: 2,
+  printWidth: 100,
+  semi: true,
+  arrowParens: 'always',
+  bracketSameLine: false,
+  trailingComma: 'all',
+  endOfLine: 'auto',
+  useTabs: false,
+};
