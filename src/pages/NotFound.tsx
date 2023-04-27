f3da7cdc819d423f8d046376165fa909e91ed5c9
Parksangwoo <psu8430@gmail.com>
2023-04-27 18:35:28 +0900

    :rotating_light: fix: linter 에러 수정

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -7,12 +7,6 @@ function NotFound() {
         <p>404</p>
         <h1>Page not found</h1>
         <p>Sorry, we couldn’t find the page you’re looking for.</p>
-        <div>
-          <a href="#">Go back home</a>
-          <a href="#">
-            Contact support <span aria-hidden="true">&rarr;</span>
-          </a>
-        </div>
       </div>
     </main>
   );
