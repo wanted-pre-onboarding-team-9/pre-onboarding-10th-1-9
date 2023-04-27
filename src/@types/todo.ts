74a2fd292e740e7c7e656dddbb6107fb98736009
sojinjang <jsojin6119@gmail.com>
2023-04-28 03:56:29 +0900

    :sparkles: feat: todo 관련 컴포넌트 타입 파일 추가

--- src/@types/todo.ts
+++ src/@types/todo.ts
@@ -1,16 +1,27 @@
+import { Ref } from 'react';
+
 export interface CheckBoxProps {
   className?: string;
+  checkboxRef: Ref<HTMLInputElement>;
   checked: boolean;
-  onClick: (e: React.MouseEvent<HTMLInputElement>) => void;
-  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
 export interface ButtonProps {
-  onClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
   text: string;
+  disabled?: boolean;
 }
 
-export interface ButtonProps {
-  onClick?: (e: React.MouseEvent<HTMLInputElement>) => void;
-  text: string;
+export interface TextInputProps {
+  className?: string;
+  name: string;
+  value: string;
+  placeholder?: string;
+  width?: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+export interface TextInputStyleProps {
+  width?: string;
 }
