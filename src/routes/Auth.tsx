a9b5ba98aabffcd99f22b0df764cb1bc8843504e
Parksangwoo <psu8430@gmail.com>
2023-04-27 19:30:21 +0900

    :adhesive_bandage: chore: 모호한 변수명 수정

--- src/routes/Auth.tsx
+++ src/routes/Auth.tsx
@@ -3,13 +3,13 @@ import { Navigate, Outlet, useLocation } from 'react-router-dom';
 import { NotFound } from '../pages';
 import { token } from '../utils';
 
-const PROTECTED_ROUTES = ['/todo'];
+const PRIVATE_ROUTES = ['/todo'];
 const PUBLIC_ROUTES = ['/', '/signin', '/signup'];
 
 function Auth() {
   const { pathname } = useLocation();
   const accessToken = token.get();
-  const isProtectedRoute = PROTECTED_ROUTES.includes(pathname);
+  const isPrivateRoute = PRIVATE_ROUTES.includes(pathname);
   const isPublicRoute = PUBLIC_ROUTES.includes(pathname);
   if (isPublicRoute) {
     if (accessToken) {
@@ -17,7 +17,7 @@ function Auth() {
     }
     return <Outlet />;
   }
-  if (isProtectedRoute) {
+  if (isPrivateRoute) {
     if (!accessToken) {
       return <Navigate to="/signin" />;
     }
