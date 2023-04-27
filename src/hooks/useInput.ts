5bf9a10a23676f0c362a3addea9696f5f3077088
sojinjang <jsojin6119@gmail.com>
2023-04-27 22:54:45 +0900

    :sparkles: feat: useInput custom hook 추가

---
+++ src/hooks/useInput.ts
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+import { AuthProps, TodoProps } from '../@types';
+
+type FormProps = AuthProps | TodoProps;
+
+export default function useInput(initialState: FormProps) {
+  const [form, setForm] = useState(initialState);
+
+  const onChangeForm = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (!e) {
+      return;
+    }
+
+    const { name, value } = e.target;
+
+    setForm({ ...form, [name]: value });
+  };
+
+  return [form, onChangeForm, setForm];
+}
