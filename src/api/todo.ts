fdf55deae06dce4c793078b927273b5e89c6342d
sojinjang <jsojin6119@gmail.com>
2023-04-28 03:51:08 +0900

    :sparkles: feat: todo api 요청함수 추가

--- src/api/todo.ts
+++ src/api/todo.ts
@@ -1,9 +1,26 @@
+import axios from 'axios';
 import ApiUrl from './ApiUrl';
 import instance from '.';
+import { TodoProps } from '../@types/response';
 
 export const getTodoData = async () => {
   const todoArr = await instance.get(ApiUrl.todo);
   return todoArr;
 };
 
-export const removeTodo = 'aa';
+export const removeTodo = async (id: number) => {
+  await instance.delete(`${ApiUrl.todo}/${id}`);
+};
+
+export const updateTodo = async (todo: TodoProps, isChecked: boolean) => {
+  await instance.put(`${ApiUrl.todo}/${todo.id}`, {
+    todo: todo.todo,
+    isCompleted: isChecked,
+  });
+};
+
+export const createTodoData = async (todo: string) => {
+  await axios.post(ApiUrl.todo, { todo }).then((res) => {
+    return res.data;
+  });
+};
