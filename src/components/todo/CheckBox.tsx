b56f52543421c811ba958bf018c64d9bc478fd4e
sojinjang <jsojin6119@gmail.com>
2023-04-28 01:18:21 +0900

    :sparkles: feat: 체크박스 추가

--- src/components/todo/CheckBox.tsx
+++ src/components/todo/CheckBox.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 import { CheckBoxProps } from '../../@types/todo';
 import CheckBoxContainer from './style';
 
-function CheckBox({ className, checked, onClick }: CheckBoxProps) {
+const CheckBox = ({ className, checked, onClick }: CheckBoxProps) => {
   return (
     <CheckBoxContainer className={className}>
       <input type="checkbox" onClick={onClick} checked={checked} />
     </CheckBoxContainer>
   );
-}
+};
 
 export default CheckBox;
