c600361d7b86f13bf43b1ad8a44a1232834f56b8
sojinjang <jsojin6119@gmail.com>
2023-04-28 01:21:21 +0900

    :sparkles: feat: 버튼 타입 추가

--- src/@types/todo.ts
+++ src/@types/todo.ts
@@ -3,3 +3,8 @@ export interface CheckBoxProps {
   checked: boolean;
   onClick?: (e: React.MouseEvent<HTMLInputElement>) => void;
 }
+
+export interface ButtonProps {
+  onClick?: (e: React.MouseEvent<HTMLInputElement>) => void;
+  text: string;
+}
