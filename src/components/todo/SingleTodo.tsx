d27d83d420c4cb68a69d6d77373123cd81228c2f
sojinjang <jsojin6119@gmail.com>
2023-04-28 06:14:23 +0900

    :sparkles: feat: todo list 수정, 삭제 기능 추가

---
+++ src/components/todo/SingleTodo.tsx
@@ -0,0 +1,79 @@
+import { TodoProps } from '../../@types/response';
+import TextInput from './TextInput';
+import CheckBox from './CheckBox';
+import { updateTodo } from '../../api/todo';
+import Button from './Button';
+
+interface SingleTodoProps {
+  todo: TodoProps;
+  updateRevisedTodo: (targetId: number, newTodo: TodoProps) => void;
+  onClickDelete: (id: number) => void;
+  form: TodoProps;
+  onChangeForm: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  setForm: React.Dispatch<React.SetStateAction<TodoProps>>;
+  clear: () => void;
+}
+
+const SingleTodo = ({
+  todo,
+  updateRevisedTodo,
+  onClickDelete,
+  form,
+  onChangeForm,
+  setForm,
+  clear,
+}: SingleTodoProps) => {
+  const onUpdateTodo = async (data: TodoProps) => {
+    await updateTodo(data);
+    if (data?.id) updateRevisedTodo(data.id, data);
+    clear();
+  };
+
+  return (
+    <li key={todo.id} style={{ display: 'flex', flexDirection: 'row' }}>
+      <CheckBox
+        checked={todo.isCompleted}
+        onClick={() => {
+          onUpdateTodo({
+            id: todo?.id,
+            todo: todo?.todo,
+            isCompleted: !todo?.isCompleted,
+            userId: todo?.userId,
+          });
+        }}
+      />
+      {form?.id === todo?.id ? (
+        <TextInput name="todo" value={form?.todo} onChange={onChangeForm} />
+      ) : (
+        <p>{todo?.todo}</p>
+      )}
+      <div style={{ marginLeft: 'auto' }}>
+        <Button
+          text={form?.id === todo?.id ? '제출' : '수정'}
+          onClick={() => {
+            if (form?.id === todo?.id) {
+              onUpdateTodo(form);
+              clear();
+            } else {
+              setForm({
+                id: todo?.id,
+                todo: todo?.todo,
+                isCompleted: todo?.isCompleted,
+                userId: todo?.userId,
+              });
+            }
+          }}
+        />
+        <Button
+          text={form?.id === todo?.id ? '취소' : '삭제'}
+          onClick={() => {
+            if (form?.id === todo?.id) {
+              clear();
+            } else if (todo.id) onClickDelete(todo.id);
+          }}
+        />
+      </div>
+    </li>
+  );
+};
+export default SingleTodo;
--- src/pages/TodoList.tsx
+++ src/pages/TodoList.tsx
@@ -1,16 +1,23 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 import { TodoProps } from '../@types/response';
 import SingleTodo from '../components/todo/SingleTodo';
+import CreateTodo from '../components/todo/CreateTodo';
+import { removeTodo } from '../api/todo';
+import useInput from '../hooks/useInput';
 
 const TodoList = () => {
-  const [editingTodoId, setEditingTodoId] = useState<number | null>(null);
   const [todos, setTodos] = useState<TodoProps[] | null>(useLoaderData() as TodoProps[]);
-
-  const filterDeletedTodo = (targetId: number) => {
-    setTodos((prevTodos) => {
-      if (!prevTodos) return prevTodos;
-      return prevTodos.filter((prevTodo) => prevTodo.id !== targetId);
+  const { form, onChangeForm, setForm, clear } = useInput({
+    id: undefined,
+    todo: '',
+    isCompleted: false,
+    userId: 0,
+  });
+  const addNewTodo = (newTodo: TodoProps) => {
+    setTodos((prev) => {
+      if (!prev) return prev;
+      return [...prev, newTodo];
     });
   };
   const updateRevisedTodo = (targetId: number, newTodo: TodoProps) => {
@@ -26,12 +33,34 @@ const TodoList = () => {
     });
   };
 
+  const onClickDelete = async (id: number) => {
+    await removeTodo(id);
+    setTodos((prevTodos) => {
+      if (!prevTodos) return prevTodos;
+      return prevTodos.filter((prevTodo) => prevTodo.id !== id);
+    });
+  };
+
   return (
-    <div>
-      {todos &&
-        todos.map((todo: TodoProps) => {
-          return <SingleTodo editingTodoId={editingTodoId} todoData={todo} />;
-        })}
+    <div style={{ display: 'flex' }}>
+      <div style={{ margin: 'auto' }}>
+        <CreateTodo addNewTodo={addNewTodo} />
+        <ul style={{ paddingLeft: '0px' }}>
+          {todos &&
+            todos?.map((todo: TodoProps) => (
+              <SingleTodo
+                key={todo.id}
+                todo={todo}
+                updateRevisedTodo={updateRevisedTodo}
+                onClickDelete={onClickDelete}
+                form={form}
+                onChangeForm={onChangeForm}
+                setForm={setForm}
+                clear={clear}
+              />
+            ))}
+        </ul>
+      </div>
     </div>
   );
 };
