308965e4ddd3ab5c2cf78085f4d06cccfc85eefa
Hadam Cho <chohadam49@gmail.com>
2023-04-28 20:52:58 +0900

    :label: feat: Response type 정의

--- src/@types/response.ts
+++ src/@types/response.ts
@@ -1,3 +1,16 @@
+import { Todo } from './todo';
+
 export interface SignInResponse {
   access_token: string;
 }
+
+export type SignUpResponse = null;
+export interface CreateTodoResponse {
+  todo: Todo['todo'];
+}
+
+export type GetTodosReponse = Todo[];
+
+export type UpdateTodoResponse = Todo;
+
+export type DeleteTodoResponse = null;
