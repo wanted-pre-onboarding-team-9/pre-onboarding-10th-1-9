edf4728408b2b6a1edf0ebde50d7c2862608d93e
Hadam Cho <chohadam49@gmail.com>
2023-04-28 19:35:34 +0900

    :fire: chore: todoLoader에서 alert 코드 제거

--- src/routes/todoLoader.ts
+++ src/routes/todoLoader.ts
@@ -1,11 +1,5 @@
 import { getTodoData } from '../api/todo';
 
-export default async function todoLoader() {
-  try {
-    const todoArr = await getTodoData();
-    return todoArr;
-  } catch (err) {
-    if (err instanceof Error) alert(err.message);
-  }
-  return undefined;
-}
+const todoLoader = () => getTodoData();
+
+export default todoLoader;
