91b63aab738f7a5a34dc6860d247c78358cf377b
Minji Jeon <104840243+mjjeon2645@users.noreply.github.com>
2023-04-28 14:35:53 +0900

    :sparkles: feat: 메인 페이지에 로그인/회원가입 이동버튼 생성 및 적용 (#17)

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,7 +1,26 @@
 import React from 'react';
 
+import { useNavigate } from 'react-router-dom';
+
+import * as S from './style';
+
 const Main = () => {
-  return <div>main page</div>;
+  const navigate = useNavigate();
+  return (
+    <S.Wrapper>
+      <S.Image
+        src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+        alt="signin"
+      />
+      <S.MainTitle>To-Do List</S.MainTitle>
+      <S.MainButton type="button" name="signin" onClick={() => navigate('/signin')}>
+        로그인 하러가기
+      </S.MainButton>
+      <S.MainButton type="button" name="signup" onClick={() => navigate('/signup')}>
+        회원가입 하러가기
+      </S.MainButton>
+    </S.Wrapper>
+  );
 };
 
 export default Main;
--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -21,7 +21,7 @@ const SignIn = () => {
 
   return (
     <S.Wrapper>
-      <S.MainImage
+      <S.Image
         src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
         alt="signin"
       />
