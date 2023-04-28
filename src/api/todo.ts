3f7679343edd91be1409dde5a9c267d327145765
Hadam Cho <chohadam49@gmail.com>
2023-04-28 21:06:46 +0900

    :label: chore: api 함수들에 response 타입 추가

--- src/api/todo.ts
+++ src/api/todo.ts
@@ -1,24 +1,30 @@
 import ApiUrl from './ApiUrl';
 import instance from '.';
+import {
+  CreateTodoResponse,
+  DeleteTodoResponse,
+  GetTodosReponse,
+  UpdateTodoResponse,
+} from '../@types/response';
 import { Todo } from '../@types/todo';
 
 export const getTodoData = async () => {
-  const { data } = await instance.get<Todo[]>(ApiUrl.todo);
+  const { data } = await instance.get<GetTodosReponse>(ApiUrl.todo);
   return data;
 };
 
-export const removeTodo = async (id: number) => {
-  await instance.delete(`${ApiUrl.todo}/${id}`);
+export const removeTodo = async (id: Todo['id']) => {
+  await instance.delete<DeleteTodoResponse>(`${ApiUrl.todo}/${id}`);
 };
 
 export const updateTodo = async (todo: Todo) => {
-  await instance.put(`${ApiUrl.todo}/${todo.id}`, {
+  await instance.put<UpdateTodoResponse>(`${ApiUrl.todo}/${todo.id}`, {
     todo: todo.todo,
-    isCompleted: todo?.isCompleted,
+    isCompleted: todo.isCompleted,
   });
 };
 
-export const createTodoData = async (todo: string) => {
-  const { data } = await instance.post(ApiUrl.todo, { todo });
+export const createTodoData = async (todo: Todo['todo']) => {
+  const { data } = await instance.post<CreateTodoResponse>(ApiUrl.todo, { todo });
   return data;
 };
